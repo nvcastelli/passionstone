21a56d520a0b149f004c9fcf2f26451f13881f9d
Nick <nick.castelli@brinqa.com>
2016-05-25 10:49:58 -0500

    added class icons, working on fetching class cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var deck = new Array();
+var hero = null;
+
 document.getElementById('cardReturn').onclick = function () {
 
     var name = document.getElementById('val1').value;
@@ -10,6 +13,18 @@ document.getElementById('cardReturn').onclick = function () {
         if (request.status >= 200 && request.status < 400) {
             // Success!
             var data = JSON.parse(request.responseText);
+
+            deck.sort();
+
+            for (var i = 0; i < 30; i++) {
+                if (deck[i + 2] == deck[i]) {
+                    deck.push(data[0].name);
+                }
+            }
+
+            console.log(deck);
+
+            //working on adding img to page
             var hi = data[0].img;
             console.log(hi);
 
@@ -22,6 +37,8 @@ document.getElementById('cardReturn').onclick = function () {
             node.appendChild(img);
 
             document.body.appendChild(img);
+            //done adding img to page
+
 
             //document.getElementById("pic").innerHTML = "<img src=' " + hi + " ' />";
 
@@ -43,4 +60,125 @@ function myFunction() {
     var value = e.options[e.selectedIndex].value;
     var text = e.options[e.selectedIndex].text;
     console.log(text);
+}
+
+$("#druid").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Druid";
+    console.log(hero);
+});
+
+$("#hunter").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Hunter";
+    console.log(hero);
+});
+
+$("#mage").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Mage";
+    console.log(hero);
+});
+
+$("#priest").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Priest";
+    console.log(hero);
+});
+
+$("#rogue").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Rogue";
+    console.log(hero);
+});
+
+
+$("#shaman").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Shaman";
+    console.log(hero);
+});
+
+
+$("#warlock").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Warlock";
+    console.log(hero);
+});
+
+
+$("#warrior").click(function(){
+    $("#findCard").show(); // or .fadeIn(), etc.
+    $("#dropVal").hide();
+    $("#classChoice").hide();
+    hero = "Warrior";
+    console.log(hero);
+});
+
+document.getElementById('classChoice').onclick = function () {
+
+    //var name = document.getElementById('val1').value;
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/' + hero, true);
+    request.setRequestHeader("X-Mashape-Key", "1T4oVFrWf4mshDSH9v7n6mAi5fz2p1rEjVmjsnN5U8VwJBvZQd");
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var data = JSON.parse(request.responseText);
+
+            deck.sort();
+
+            for (var i = 0; i < 30; i++) {
+                if (deck[i + 2] == deck[i]) {
+                    deck.push(data[0].name);
+                }
+            }
+
+            console.log(deck);
+
+            //working on adding img to page
+            var hi = data[0].img;
+            console.log(hi);
+
+            var node = document.createElement("img");
+            node.href = hi;
+            node.alt = "ass";
+
+            var img = document.createElement("img");
+            img.src = hi;
+            node.appendChild(img);
+
+            document.body.appendChild(img);
+            //done adding img to page
+
+
+            //document.getElementById("pic").innerHTML = "<img src=' " + hi + " ' />";
+
+        } else {
+            // We reached our target server, but it returned an error
+        }
+    };
+
+    request.onerror = function () {
+        // There was a connection error of some sort
+    };
+
+    request.send();
+
 }
